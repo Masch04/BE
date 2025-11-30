99645a359e2d26303deebf122d02d81c81acf13f
Masch04 <masch04@gmail.com>
2025-11-30 16:23:41 +0700

    sedeer

--- database/seeders/DichVuSeeder.php
+++ database/seeders/DichVuSeeder.php
@@ -15,10 +15,10 @@ class DichVuSeeder extends Seeder
     {
         DB::table('dich_vus')->delete();
         DB::table('dich_vus')->insert([
-            ['id' => '1' ,'ten_dich_vu' => 'Đưa đón sân bay', 'don_gia' => '500', 'don_vi_tinh' => 'Dịch Vụ', 'ghi_chu' => 'Xe đưa đón tận nơi, phục vụ 24/7.', 'tinh_trang' => '1'],
-            ['id' => '2' ,'ten_dich_vu' => 'Dọn phòng hàng ngày', 'don_gia' => '300', 'don_vi_tinh' => 'Dịch Vụ', 'ghi_chu' => 'Dọn dẹp, thay khăn và sắp xếp phòng mỗi ngày.', 'tinh_trang' => '1'],
-            ['id' => '3' ,'ten_dich_vu' => 'Giặt ủi', 'don_gia' => '200 ', 'don_vi_tinh' => 'Dịch Vụ', 'ghi_chu' => 'Giặt, sấy và ủi quần áo nhanh chóng.', 'tinh_trang' => '1'],
-            ['id' => '4' ,'ten_dich_vu' => 'Thuê nhiếp ảnh', 'don_gia' => ' 600 ', 'don_vi_tinh' => 'Người', 'ghi_chu' => 'Chụp ảnh chuyên nghiệp theo yêu cầu.', 'tinh_trang' => '1'],
+            ['id' => '1' ,'ten_dich_vu' => 'Đưa đón sân bay', 'don_gia' => '500000', 'don_vi_tinh' => 'Dịch Vụ', 'ghi_chu' => 'Xe đưa đón tận nơi, phục vụ 24/7.', 'tinh_trang' => '1'],
+            ['id' => '2' ,'ten_dich_vu' => 'Dọn phòng hàng ngày', 'don_gia' => '300000', 'don_vi_tinh' => 'Dịch Vụ', 'ghi_chu' => 'Dọn dẹp, thay khăn và sắp xếp phòng mỗi ngày.', 'tinh_trang' => '1'],
+            ['id' => '3' ,'ten_dich_vu' => 'Giặt ủi', 'don_gia' => '200000 ', 'don_vi_tinh' => 'Dịch Vụ', 'ghi_chu' => 'Giặt, sấy và ủi quần áo nhanh chóng.', 'tinh_trang' => '1'],
+            ['id' => '4' ,'ten_dich_vu' => 'Thuê nhiếp ảnh', 'don_gia' => ' 600000 ', 'don_vi_tinh' => 'Người', 'ghi_chu' => 'Chụp ảnh chuyên nghiệp theo yêu cầu.', 'tinh_trang' => '1'],
         ]);
     }
 }
